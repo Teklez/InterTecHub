a0325bf7d7b44ee64bc2704fa9ef22741757ee04
Teklez <zemenumekiria@gmail.com>
2024-11-17 22:58:27 +0300

    fix():corrected typo in app.js prompt -> port

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-
-router.get("/name", (req, res)=>{
-    res.send("Zemenu Mekuria")
-})
+router.get("/name", (req, res) => {
+  res.send("Zemenu Mekuria");
+});
 
 router.get("/hobby", (req, res) => {
-    res.send("I like reading in my free time")
-})
+  res.json({
+    hobby: "Reading",
+  });
+});
 
-router.get("/dream", (req, res)=>{
-    
-})
+router.get("/dream", (req, res) => {
+  res.send("Death smiles at us all, all man can do is smile back");
+});
 
 module.exports = router;
-
-
