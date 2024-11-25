0b7df86537d1937b6b026690c418ebc85239e4a4
Teklez <zemenumekiria@gmail.com>
2024-11-25 23:21:15 +0300

    feat():add PUT /books/:id endpoint

--- api/books.js
+++ api/books.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-import { body, validationResult } from "express-validator";
+import { body, param, validationResult } from "express-validator";
 
 const prisma = new PrismaClient();
 
@@ -16,7 +16,7 @@ booksRouter.get("/books", async (req, res) => {
         message: "No books found",
       });
     } else {
-      res.json({
+      res.status(200).json({
         books: books,
       });
     }
@@ -35,6 +35,7 @@ booksRouter.post(
   body("author").notEmpty().isLength({ min: 3, max: 30 }),
   body("isbn")
     .isISBN()
+    .notEmpty()
     .custom(async (value) => {
       const bookExists = await prisma.book.findUnique({
         where: {
@@ -45,7 +46,7 @@ booksRouter.post(
         throw new Error("A book with this ISBN already exists.");
       }
     }),
-  body("publishedYear").isLength({ min: 4, max: 4 }),
+  body("publishedYear").notEmpty().isLength({ min: 4, max: 4 }),
 
   async (req, res) => {
     // result of validation chain
@@ -69,6 +70,57 @@ booksRouter.post(
       });
     } catch (error) {
       console.error("Error creating book:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+);
+
+// UPDATE book by id
+booksRouter.put(
+  "/books/:id",
+  // validation chain
+  body("title").notEmpty().isLength({ min: 3, max: 30 }),
+  body("author").notEmpty().isLength({ min: 3, max: 30 }),
+  body("isbn").notEmpty().isISBN(),
+  body("publishedYear").notEmpty().isLength({ min: 4, max: 4 }),
+  param("id")
+    .notEmpty()
+    .custom(async (value) => {
+      const bookExists = await prisma.book.findUnique({
+        where: {
+          id: parseInt(value),
+        },
+      });
+      if (!bookExists) {
+        throw new Error("A book with this id doesn't exists.");
+      }
+    }),
+
+  async (req, res) => {
+    // result of validation chain
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      await prisma.book.update({
+        where: {
+          id: parseInt(req.params.id),
+        },
+        data: {
+          title: req.body.title,
+          author: req.body.author,
+          isbn: req.body.isbn,
+          publishedYear: req.body.publishedYear,
+        },
+      });
+
+      res.status(200).json({
+        message: "The book is successfully Updated.",
+      });
+    } catch (error) {
+      console.error("Error Updating book:", error);
       res.status(500).json({ message: "Internal server error" });
     }
   }
